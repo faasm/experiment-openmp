7e07bcad9ed92e2d74838ed922d4b2b5fcb9d5c6
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-03-24 13:48:53 +0000

    wip

--- run/single_native.py
+++ run/single_native.py
@@ -80,6 +80,25 @@ def run_single_src(country, num_omp_threads, debug):
     print("\t- Country: {}".format(country))
     print("\t- Num. OMP Threads: {}".format(num_omp_threads))
 
+    # Bit copied from the original source 
+    # Lists of places that need to be handled specially
+    united_states = [ "United_States" ]
+    canada = [ "Canada" ]
+    usa_territories = ["Alaska", "Hawaii", "Guam", "Virgin_Islands_US", "Puerto_Rico", "American_Samoa"]
+    nigeria = ["Nigeria"]
+    # Population density file in gziped form, text file, and binary file as
+    # processed by CovidSim
+    if country in united_states + canada:
+        wpop_file_root = "usacan"
+    elif country in usa_territories:
+        wpop_file_root = "us_terr"
+    elif country in nigeria:
+        wpop_file_root = "nga_adm1"
+    else:
+        wpop_file_root = "eur"
+    wpop_file = "wpop_{}.txt".format(wpop_file_root)
+    # End of paraphrased bit
+
     # Prepare cmd
     _cmd = [
         COVID_SIM_BUILD_PATH,
@@ -88,7 +107,7 @@ def run_single_src(country, num_omp_threads, debug):
         "/PP:{}/param_files/preUK_R0=2.0.txt".format(DATA_DIR),
         "/P:{}/param_files/p_NoInt.txt".format(DATA_DIR),
         "/O:/tmp/{}_NoInt_R0=3.0".format(country),
-        "/D:/tmp/wpop_us_terr.txt",
+        "/D:/tmp/{}".format(wpop_file),
         "/M:/tmp/{}_pop_density.bin".format(country),
         "/S:/tmp/Network_{}_T1_R3.0.bin".format(country),
         "/R:1.5 98798150 729101 17389101 4797132",

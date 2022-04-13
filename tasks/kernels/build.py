ef7b2d8126029af5340a8a505b5f75f08160eeee
Simon Shillaker <mail@simonshillaker.com>
2022-04-13 13:45:57 +0000

    Copy wasm files into place

--- tasks/kernels/build.py
+++ tasks/kernels/build.py
@@ -1,9 +1,12 @@
 from invoke import task
-from os.path import join
+from os.path import join, exists
+from os import makedirs
 from subprocess import run
+import shutil
 import requests
 
 from tasks.faasm import get_faasm_upload_host_port
+from tasks.util import FAASM_LOCAL_DIR
 from tasks.kernels.env import (
     KERNELS_NATIVE_DIR,
     KERNELS_WASM_DIR,
@@ -28,7 +31,8 @@ def native(ctx, clean=False):
     """
     Build native kernels
     """
-    _do_build(KERNELS_NATIVE_DIR, clean)
+    print("Running kernels native build in {}".format(KERNELS_NATIVE_DIR))
+    _do_build(KERNELS_NATIVE_DIR, clean, False)
 
 
 @task
@@ -36,15 +40,37 @@ def wasm(ctx, clean=False):
     """
     Build kernels to wasm
     """
-    _do_build(KERNELS_WASM_DIR, clean)
+    print("Running kernels wasm build in {}".format(KERNELS_WASM_DIR))
+    _do_build(KERNELS_WASM_DIR, clean, True)
 
 
-def _do_build(src_dir, clean):
+def _do_build(src_dir, clean, is_wasm):
     if clean:
+        print("Cleaning {}".format(src_dir))
         run("make clean", shell=True, cwd=src_dir)
 
-    for src_dir, target in SUPPORTED_KERNELS:
-        run("make {}".format(target), shell=True, cwd=src_dir)
+    for relative_dir, target in SUPPORTED_KERNELS:
+        work_dir = join(src_dir, relative_dir)
+        print("Making {} in {}".format(target, work_dir))
+        run("make {}".format(target), shell=True, cwd=work_dir)
+
+        # Copy wasm file into place locally
+        if is_wasm:
+            local_dir = join(
+                FAASM_LOCAL_DIR, "wasm", KERNELS_FAASM_USER, target
+            )
+
+            if not exists(local_dir):
+                print("Creating {}".format(local_dir))
+                makedirs(local_dir)
+
+            local_file = join(local_dir, "function.wasm")
+            wasm_file = join(
+                KERNELS_WASM_DIR, "wasm", "{}.wasm".format(target)
+            )
+
+            print("Copying {} to {}".format(wasm_file, local_file))
+            shutil.copy(wasm_file, local_file)
 
 
 @task
@@ -54,7 +80,7 @@ def upload(ctx):
     """
     host, port = get_faasm_upload_host_port()
 
-    for src_dir, target in SUPPORTED_KERNELS:
+    for rel_dir, target in SUPPORTED_KERNELS:
         wasm_file = join(KERNELS_WASM_DIR, "wasm", "{}.wasm".format(target))
 
         url = "http://{}:{}/f/{}/{}".format(

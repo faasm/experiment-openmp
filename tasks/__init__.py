2d52352e96e11aa4c9c2dd063ad6c073daa0fbb9
Simon Shillaker <mail@simonshillaker.com>
2022-04-14 12:52:30 +0000

    More removal

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -5,7 +5,6 @@ from . import container
 import logging
 
 from tasks.covid import ns as covid_ns
-from tasks.john import ns as john_ns
 from tasks.kernels import ns as kernels_ns
 from tasks.lulesh import ns as lulesh_ns
 
@@ -16,6 +15,5 @@ ns = Collection(
 )
 
 ns.add_collection(covid_ns, name="covid")
-ns.add_collection(john_ns, name="john")
 ns.add_collection(kernels_ns, name="kernels")
 ns.add_collection(lulesh_ns, name="lulesh")
